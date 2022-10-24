fdb461e9fe282691a35900122c396a1b6a981641
biud436 <biud436@gmail.com>
2022-10-24 17:37:54 +0900

    feat: added SyntaxHighlighter

--- src/app/components/post/PostContent.tsx
+++ src/app/components/post/PostContent.tsx
@@ -1,11 +1,28 @@
 import { Post } from '@/store/post';
 import { Grid } from '@mui/material';
 import Markdown from 'marked-react';
+import ReactRenderer from 'marked-react/dist/ReactRenderer';
+import { ReactNode } from 'react';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+
+type Optional<T> = {
+    [P in keyof T]?: T[P];
+};
+
+const CodeRenderer: Optional<ReactRenderer> = {
+    code(snippet: ReactNode, lang: string | undefined) {
+        return (
+            <SyntaxHighlighter key={this.elementId} language={lang}>
+                {snippet}
+            </SyntaxHighlighter>
+        );
+    },
+};
 
 export function PostContent({ post }: { post: Post }) {
     return (
         <Grid item xs={12}>
-            <Markdown>{post.content}</Markdown>
+            <Markdown value={post.content} renderer={CodeRenderer} />
         </Grid>
     );
 }
