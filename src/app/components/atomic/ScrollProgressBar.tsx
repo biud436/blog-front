ae4930416e086cb510d219744b028730327adc26
biud436 <biud436@gmail.com>
2022-11-21 11:36:57 +0900

    feat: 스크롤바 모션 추가

--- src/app/components/atomic/ScrollProgressBar.tsx
+++ src/app/components/atomic/ScrollProgressBar.tsx
@@ -2,6 +2,8 @@ import { motion, useScroll, useSpring } from 'framer-motion';
 import { useEffect } from 'react';
 import { createGlobalStyle } from 'styled-components';
 import ReactDOM from 'react-dom';
+import { useRouter } from 'next/router';
+import { URL_MAP } from '@/common/URL';
 
 const ProgressBarWrapper = createGlobalStyle`
     .progress-bar {
@@ -10,19 +12,25 @@ const ProgressBarWrapper = createGlobalStyle`
         left: 0;
         right: 0;
         height: 5px;
-        background: #66aaff;
+        /* background: #66aaff; */
+        background: #ff0000;
         transform-origin: 0%;
         z-index: 9999;
     }
 `;
 
 export const ScrollProgressBar = () => {
+    const router = useRouter();
     const { scrollYProgress } = useScroll();
     const scaleX = useSpring(scrollYProgress, {
         stiffness: 100,
         damping: 30,
         restDelta: 0.001,
     });
+
+    if (router.pathname === URL_MAP.MAIN) {
+        return null;
+    }
 
     return (
         <>
