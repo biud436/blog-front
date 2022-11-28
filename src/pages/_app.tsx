55a1694e8d1c3f40843e80c1f6b2d7f3eab61dfb
biud436 <biud436@gmail.com>
2022-11-28 11:05:34 +0900

    feat: CSS Reset 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { RecoilRoot } from 'recoil';
 import { Noto_Sans_KR } from '@next/font/google';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { CssBaseline } from '@mui/material';
 
 const notoSansKR = Noto_Sans_KR({
     weight: '100',
@@ -17,6 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
     return (
         <main className={notoSansKR.className}>
             <RecoilRoot>
+                <CssBaseline />
                 <GlobalStyle />
                 <CategoryServiceProvider>
                     <PostServiceProvider>
