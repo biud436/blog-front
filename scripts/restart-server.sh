c799512e128606fcd45ee6931d9d10a5c9570e92
biud436 <biud436@gmail.com>
2023-03-19 15:44:12 +0900

    fix: 도커 이미지 반영 후, 용량 확보

--- scripts/restart-server.sh
+++ scripts/restart-server.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 cd /home/ubuntu/devops
 sudo docker-compose pull blog
+sleep 10
 sudo docker-compose up -d blog
-# sudo docker image prune -f
+sleep 10
+echo 'y' | sudo docker image prune -a
