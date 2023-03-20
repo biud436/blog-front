5464e377c5f565c496fe2ec167eee9eac95905e1
biud436 <biud436@gmail.com>
2023-03-20 22:11:08 +0900

    feat: added usePostsService2

--- src/hooks/usePostsService.ts
+++ src/hooks/usePostsService.ts
@@ -1,4 +1,10 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { API_URL } from '@/blog/api/request';
 import { PostsServiceContext, IPostsService } from '@/services/PostsService';
+import { ReactServiceStore } from '@/services/types/ReactServiceStore';
+import { PostsSearchType } from '@/store/posts/posts.dto';
+import axios from 'axios';
 import { useContext } from 'react';
 
 /**
@@ -16,4 +22,61 @@ export function usePostsService(): IPostsService {
     }
 
     return service;
+}
+
+export function usePostsService2(): IPostsService {
+    const search = async (
+        pageNumber: number,
+        searchProperty: string,
+        searchQuery: string,
+    ) => {
+        const res = await axios.get(
+            `${API_URL}/posts/search?pageNumber=${pageNumber}&searchProperty=${searchProperty}&searchQuery=${encodeURIComponent(
+                searchQuery,
+            )}`,
+        );
+
+        return res.data;
+    };
+
+    const view = async (
+        pageNumber: number,
+        categoryId?: number,
+    ): Promise<any> => {
+        const res = await axios.get(
+            `${API_URL}/posts?page=${pageNumber}&categoryId=${categoryId}`,
+        );
+
+        return res.data;
+    };
+
+    const fetch = (
+        store: ReactServiceStore<PostsSearchType>,
+        categoryId?: number,
+    ): Promise<any> => {
+        const res = store.isSearchMode()
+            ? search(
+                  store.getPageNumber(),
+                  store.getSearchType()!,
+                  store.getSearchQuery()!,
+              )
+            : view(store.getPageNumber(), categoryId);
+
+        return res;
+    };
+
+    const getBreadcrumb = async (categoryName: string): Promise<any> => {
+        const res = await axios.get(
+            `${API_URL}/posts/breadcrumbs?categoryName=${categoryName}`,
+        );
+
+        return res.data;
+    };
+
+    return {
+        fetch,
+        getBreadcrumb,
+        search,
+        view,
+    };
 }
