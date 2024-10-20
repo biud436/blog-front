479d14c9695f6628f2254ba800f599f90c492388
biud436 <biud436@gmail.com>
2024-10-20 21:08:35 +0900

    refactor: Update PostFooter component and PostEditorContainer
    - Refactor PostFooter component to import useAuthorized from '@/hooks/server/useAuthorized' instead of '@/hooks/auth/useAuthorized'
    - Refactor PostEditorContainer to import useAuthorized from '@/hooks/server/useAuthorized' instead of '@/hooks/auth/useAuthorized'

--- src/blog/components/post/PostFooter.tsx
+++ src/blog/components/post/PostFooter.tsx
@@ -1,86 +1,74 @@
 import React from 'react';
 import { Button, Grid } from '@mui/material';
-import { useAuthorized } from '@/hooks/auth/useAuthorized';
 import { usePostService } from '@/hooks/services/usePostService';
 import { toast } from 'react-toastify';
 import { Post } from '@/models/Post';
 import { useRouter } from 'next/navigation';
+import { useAuthorized } from '@/hooks/server/useAuthorized';
 
 export function PostFooter({
-    post,
-    goBack,
+  post,
+  goBack,
 }: {
-    post: Post;
-    goBack: () => void;
+  post: Post;
+  goBack: () => void;
 }) {
-    const router = useRouter();
-    const [isAuthorized] = useAuthorized();
-    const postService = usePostService();
+  const router = useRouter();
+  const { isAuthorized } = useAuthorized();
+  const postService = usePostService();
 
-    const handleDeletePost = async () => {
-        const currentPostId = post.id;
+  const handleDeletePost = async () => {
+    const currentPostId = post.id;
 
-        if (confirm('정말 삭제하시겠습니까?')) {
-            const res = await postService.deletePost(currentPostId);
+    if (confirm('정말 삭제하시겠습니까?')) {
+      const res = await postService.deletePost(currentPostId);
 
-            if (res.result === 'success') {
-                router.push('/');
-            } else {
-                toast.error('삭제에 실패했습니다.');
-            }
-        }
-    };
+      if (res.result === 'success') {
+        router.push('/');
+      } else {
+        toast.error('삭제에 실패했습니다.');
+      }
+    }
+  };
 
-    const handleEditPost = () => {
-        const id = post.id;
-        router.push(`/edit2?mode=edit&id=${id}`);
-    };
+  const handleEditPost = () => {
+    const id = post.id;
+    router.push(`/edit2?mode=edit&id=${id}`);
+  };
 
-    return (
-        <Grid
-            container
-            spacing={0}
-            direction="row"
-            alignItems="center"
-            justifyContent={'space-between'}
-        >
-            <Grid item xs={8}>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={() => goBack()}
-                >
-                    이전
-                </Button>
-            </Grid>
-            {isAuthorized && (
-                <Grid container spacing={0}>
-                    <Grid
-                        item
-                        xs={12}
-                        gap={2}
-                        sx={{
-                            display: 'flex',
-                            justifyContent: 'end',
-                        }}
-                    >
-                        <Button
-                            variant="outlined"
-                            color="warning"
-                            onClick={handleEditPost}
-                        >
-                            수정
-                        </Button>
-                        <Button
-                            variant="outlined"
-                            color="error"
-                            onClick={handleDeletePost}
-                        >
-                            삭제
-                        </Button>
-                    </Grid>
-                </Grid>
-            )}
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="row"
+      alignItems="center"
+      justifyContent={'space-between'}
+    >
+      <Grid item xs={8}>
+        <Button variant="contained" color="primary" onClick={() => goBack()}>
+          이전
+        </Button>
+      </Grid>
+      {isAuthorized && (
+        <Grid container spacing={0}>
+          <Grid
+            item
+            xs={12}
+            gap={2}
+            sx={{
+              display: 'flex',
+              justifyContent: 'end',
+            }}
+          >
+            <Button variant="outlined" color="warning" onClick={handleEditPost}>
+              수정
+            </Button>
+            <Button variant="outlined" color="error" onClick={handleDeletePost}>
+              삭제
+            </Button>
+          </Grid>
         </Grid>
-    );
+      )}
+    </Grid>
+  );
 }
--- src/containers/PostEditorContainer.tsx
+++ src/containers/PostEditorContainer.tsx
@@ -3,7 +3,6 @@ import { Grid, Paper, Alert } from '@mui/material';
 import { observer } from 'mobx-react-lite';
 import { MainLayout } from '@/layouts/BlogMainLayout';
 import { useEffect, useRef } from 'react';
-import { useAuthorized } from '@/hooks/auth/useAuthorized';
 import dynamic from 'next/dynamic';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -12,88 +11,89 @@ import FlexibleLoading from '@/blog/components/common/FlexibleLoading';
 import { PostEditorPageHeader } from './PostEditorPageHeader';
 import { PostEditorPageDescription } from './PostEditorPageDescription';
 import { PostServiceProvider } from '@/services/PostService';
+import { useAuthorized } from '@/hooks/server/useAuthorized';
 
 export type EditMode = 'create' | 'edit';
 export interface EditPageProps {
-    mode: EditMode;
-    id?: number;
+  mode: EditMode;
+  id?: number;
 }
 
 const PostEditorPresent = dynamic(
-    async () => {
-        const [mod] = await Promise.all([
-            import('../blog/components/post/editor/PostEditorPresent'),
-        ]);
+  async () => {
+    const [mod] = await Promise.all([
+      import('../blog/components/post/editor/PostEditorPresent'),
+    ]);
 
-        return mod.PostEditorPresent;
-    },
-    {
-        ssr: false,
-        loading: () => <FlexibleLoading />,
-    },
+    return mod.PostEditorPresent;
+  },
+  {
+    ssr: false,
+    loading: () => <FlexibleLoading />,
+  },
 );
 
 export const PostEditor = observer(({ mode }: EditPageProps) => {
-    return (
-        <PostServiceProvider>
-            <PostEditorPresent mode={mode} />
-        </PostServiceProvider>
-    );
+  return (
+    <PostServiceProvider>
+      <PostEditorPresent mode={mode} />
+    </PostServiceProvider>
+  );
 });
 
 export const PostEditorContainer = observer(
-    ({ editorMode }: { editorMode: string }) => {
-        const [isAuthorized] = useAuthorized();
+  ({ editorMode }: { editorMode: string }) => {
+    const { isAuthorized } = useAuthorized();
 
-        // useState로 할 경우, 렌더링이 두 번 일어나면서 버그가 발생합니다.
-        const mode = useRef<EditPageProps['mode']>('create');
+    // useState로 할 경우, 렌더링이 두 번 일어나면서 버그가 발생합니다.
+    const mode = useRef<EditPageProps['mode']>('create');
 
-        const LoginGuard = ({ children }: { children: JSX.Element }) => {
-            return !isAuthorized ? (
-                <Grid
-                    container
-                    sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Grid item xs={12}>
-                        <Alert variant="filled" severity="error">
-                            로그인이 필요한 서비스입니다
-                        </Alert>
-                    </Grid>
-                </Grid>
-            ) : (
-                children
-            );
-        };
+    const LoginGuard = ({ children }: { children: JSX.Element }) => {
+      return !isAuthorized ? (
+        <Grid
+          container
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Grid item xs={12}>
+            <Alert variant="filled" severity="error">
+              로그인이 필요한 서비스입니다
+            </Alert>
+          </Grid>
+        </Grid>
+      ) : (
+        children
+      );
+    };
 
-        useEffect(() => {
-            if (editorMode === 'edit') {
-                mode.current = 'edit';
-            }
-        }, []);
+    useEffect(() => {
+      if (editorMode === 'edit') {
+        mode.current = 'edit';
+      }
+    }, []);
 
-        return (
-            <MainLayout name="포스트 에디터">
-                <Meta
-                    {...{
-                        title: '포스트 에디터',
-                        description: '포스트를 작성하거나 수정합니다',
-                    }}
-                />
-                <Paper sx={{ padding: 2 }} key="editor">
-                    <LoginGuard>
-                        <Grid container gap={3}>
-                            <PostEditorPageHeader mode={mode.current} />
-                            <PostEditorPageDescription mode={mode.current} />
-                            <PostEditor mode={mode.current} />
-                        </Grid>
-                    </LoginGuard>
-                    <ToastContainer />
-                </Paper>
-            </MainLayout>
-        );
-    },
+    return (
+      <MainLayout name="포스트 에디터">
+        <Meta
+          {...{
+            title: '포스트 에디터',
+            description: '포스트를 작성하거나 수정합니다',
+          }}
+        />
+        <Paper sx={{ padding: 2 }} key="editor">
+          <LoginGuard>
+            <Grid container gap={3}>
+              <PostEditorPageHeader mode={mode.current} />
+              <PostEditorPageDescription mode={mode.current} />
+              <PostEditor mode={mode.current} />
+            </Grid>
+          </LoginGuard>
+          <ToastContainer />
+        </Paper>
+      </MainLayout>
+    );
+  },
 );
