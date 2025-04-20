095de991376cd8f66d1f4a198a2c22d164572e76
biud436 <biud436@gmail.com>
2025-04-20 20:32:37 +0900

    fix: NotFoundPage에서 경고 메시지를 영어로 변경

--- src/components/pages/NotFoundPage.tsx
+++ src/components/pages/NotFoundPage.tsx
@@ -5,7 +5,7 @@ import { Alert } from '@mui/material';
 export const NotFoundPage = () => {
   return (
     <MainLayout name="페이지 오류">
-      <Alert severity="error">페이지를 찾을 수 없습니다.</Alert>
+      <Alert severity="error">Page Not Found</Alert>
     </MainLayout>
   );
 };
